081e8ee482194e50e0119f3066de6653579e69f1
JB <you@example.comjean-baptiste.huet@epitech.eu>
2021-03-02 09:59:32 +0100

    maj bdd

---
+++ bdd/archive_joueurs_whileliste.sql
@@ -0,0 +1,74 @@
+-- --------------------------------------------------------
+-- Hôte:                         127.0.0.1
+-- Version du serveur:           10.1.47-MariaDB-0+deb9u1 - Debian 9.13
+-- SE du serveur:                debian-linux-gnu
+-- HeidiSQL Version:             11.1.0.6116
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+-- Listage des données de la table stargatedb.whitelist : ~48 rows (environ)
+-- DELETE FROM `whitelist`;
+/*!40000 ALTER TABLE `whitelist` DISABLE KEYS */;
+INSERT INTO `whitelist` (`wl_id`, `wl_pseudo`, `wl_tag`, `wl_admin_level`, `wl_uid`) VALUES
+	(1, 'AureProd', 'null', 5, '76561198291437537'),
+	(7, 'MysterFreeze', 'null', 5, '76561198006133017'),
+	(8, 'fadas', NULL, 0, '76561198165158755'),
+	(9, 'lodox', 'null', 5, '76561198025780224'),
+	(10, 'fox-alpha', NULL, 0, '76561198208475892'),
+	(11, 'ytsuka', NULL, 0, '76561198165061459'),
+	(12, 'westen', NULL, 0, '76561197995594735'),
+	(13, 'warwolf', NULL, 0, '76561199002016935'),
+	(25, 'aissa', NULL, 0, '76561198392862353'),
+	(26, 'Blackout', NULL, 0, '76561198303058318'),
+	(27, 'Foxtrot', NULL, 0, '76561198140828094'),
+	(28, 'Underwood', NULL, 0, '76561198169985342'),
+	(29, 'Aquila Mark', NULL, 0, '76561198196523477'),
+	(30, 'sylphius', NULL, 0, '76561197994740201'),
+	(31, 'Ghis', NULL, 0, '76561198098005406'),
+	(32, 'Jean Mario', NULL, 0, '76561198177449939'),
+	(33, 'Titou', NULL, 0, '76561198035762149'),
+	(34, 'black raven68', NULL, 0, '76561198066023085'),
+	(35, 'soldat ghost', NULL, 0, '76561198082245360'),
+	(36, 'Evan Keen', NULL, 0, '76561198249426408'),
+	(37, 'Zebra', NULL, 0, '76561198072211790'),
+	(38, 'crazysasa06220', NULL, 0, '76561198252972623'),
+	(39, 'French Baguette', NULL, 0, '76561198175621873'),
+	(40, 'Prosper le Purineur', NULL, 0, '76561197983265506'),
+	(41, 'Gint0x', NULL, 0, '76561197975952412'),
+	(42, 'MarcusPrimes', NULL, 0, '76561198137286812'),
+	(43, 'Mikaz66', NULL, 0, '76561198050437189'),
+	(44, 'Infinity', NULL, 0, '76561198119969039'),
+	(45, 'SpidaChris', NULL, 0, '76561198061688421'),
+	(46, 'xanax', NULL, 0, '76561197989856831'),
+	(47, 'OniHunter', NULL, 0, '76561198027552784'),
+	(48, 'Doctor Play', NULL, 0, '76561198121649847'),
+	(49, 'Alex Murphy', NULL, 0, '76561198158191510'),
+	(52, 'Ralf', NULL, 0, '76561197968387202'),
+	(53, 'opti2025', NULL, 0, '76561198084730393'),
+	(54, 'Controleur', NULL, 0, '76561198045194376'),
+	(55, 'Mateja', NULL, 0, '76561198053858627'),
+	(56, '[BYJ] Jey', NULL, 0, '76561198086170311'),
+	(57, 'Max McCarthy', NULL, 0, '76561198297962495'),
+	(58, 'Elton/225', NULL, 0, '76561198382786708'),
+	(59, 'Edouard.S.', NULL, 0, '76561198013254407'),
+	(60, 'Loann', NULL, 0, '76561198171075495'),
+	(61, 'Bob McCarthy', NULL, 0, '76561198068556406'),
+	(62, 'Caliceba', NULL, 0, '76561197968903028'),
+	(63, 'Poutchev', NULL, 0, '76561198151858167'),
+	(64, 'Yram', NULL, 0, '6561198076057832'),
+	(65, 'Quentin187', NULL, 0, '76561198084833251'),
+	(66, 'sasor14', NULL, 0, '76561198981324342'),
+	(67, 'Tribaljo (John Martous)', '#2795', 0, '76561197975282714'),
+	(68, 'Luca Cops / Sipow_', '4168', 0, '76561199015792040');
+/*!40000 ALTER TABLE `whitelist` ENABLE KEYS */;
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
